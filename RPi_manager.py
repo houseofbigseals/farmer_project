8bab4727b3a416fa7b6e74ce69d43466f86f604e
crutch_lord <slonik256@yandex.ru>
2019-03-07 04:20:17 +0300

    added unit class to RPi_manager

--- RPi_manager.py
+++ RPi_manager.py
@@ -8,7 +8,7 @@
 # - read data from schedule file on the drive
 #
 # Send data:
-# - send uart commands to GIS for control LED lamp
+# - send uart commands to GIC for control LED lamp
 # - send uart commands to SBA5
 # - set GPIO on and off to control valves, pumps and other relay things
 # - send heartbeat http requests to remote internet server and parse answers to
@@ -66,9 +66,116 @@
 # 4. Server response with command (or list of commands) again
 #
 # List of units with their tasks
+#
+# All units have following methods:
+# - get_last_task_status - result of last done operation
+# - get_list_of_methods - send list of all available methods for this unit
+# - get_unit_status - there might be some info about current unit state
+# All units can override this methods according with their essence
+#
 # 1. system_unit
 # - reset_RPi - carefully stop all systems and reset RPi through systemctl
 # - do_nothind - its server answer if server doesnt need anything from RPi
 # - list_of_units - send list of units to server
 # - get_log - send log to server
 # - get_schedule - send schedule file to server
+#
+# 2. LED_unit
+# - set Red and White currents
+# - start GIC
+# - stop GIC
+# - set_new_profile
+# - get_current_profile
+#
+# 3. ventilation_unit
+# - set_relay_N_on
+# - set_relay_N_off
+# - passively_open_system
+# - passively_close_system
+# - start_ventilation
+# - stop_ventilation
+#
+# 4. weight_unit
+# - get_current_mass
+#
+# 5. GA_unit
+# - get_current_CO2_concentration
+# - get_current_settings
+# - set_new_settings
+# - start_adjustment
+#
+# 6. temp_sensor_unit
+# - get_temp_and_humidity
+#
+#
+
+import time
+import asyncio
+import aiohttp
+
+
+class Unit:
+    """
+    Simple prototype for unit object
+    """
+    def __init__(self):
+        self.last_operation_status = None
+        self.status = None
+        pass
+
+    def get_unit_status(self) -> None:
+        """
+        There must be that logic:
+        firstly check how it is
+        and then return state, as example OK or SOME_ERROR
+        """
+        pass
+
+    def get_list_of_methods(self) -> None:
+        """
+        We have to return a list of public methods
+        Do not use dir() or hasattr(module_name, "attr_name") or inspect.getmembers
+        Only hand-written list of safe public methods to use remotely
+        """
+        pass
+
+    def get_last_task_status(self) -> None:
+        """
+        Returns status of last operation
+        """
+        pass
+
+
+class Schedule:
+    """
+    This is schedule object
+    It must read config from disc and every time be able to put out
+    current tasks for all units
+    """
+    def __init__(self, path: str = "/etc/farmer_schedule"):
+        pass
+
+    def get_current_tasks(self):
+        pass
+
+    def get_tasks(self, time_: time.struct_time):
+        pass
+
+    def add_command(self, command):
+        pass
+
+    def _read_config(self):
+        pass
+
+    def _write_config(self):
+        pass
+
+
+class RPiManager:
+    """
+    This is async manager for RPi
+    It must communicate with local and remote servers
+    And work with schedule object
+    """
+    def __init__(self):
+        pass
