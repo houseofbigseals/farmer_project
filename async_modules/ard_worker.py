2d9234710ac43cb7f118644a4e2c4664745622d1
crutch_lord <slonik256@yandex.ru>
2019-06-14 21:21:59 +0300

    added ard_worker

--- async_modules/ard_worker.py
+++ async_modules/ard_worker.py
@@ -1,14 +1,13 @@
 import time
 import asyncio
 import json
-import csv
 from uuid import uuid4
 from contextlib import suppress
 from network_modules.command import Command, Ticket
 from async_modules.tasks import SingleTask, LongSingleTask, PeriodicCoro, SingleCoro
 from network_modules.raw_client import command_request_ticket\
     , command_set_ticket_result
-from async_modules.units import SystemUnit, LedUnit, CO2SensorUnit, WeightUnit, TempSensorUnit, GpioUnit, K30Unit
+from async_modules.units import SystemUnit, ArdWrapper
 import logging
 
 logger = logging.getLogger("Worker._Worker")
@@ -77,28 +76,23 @@ class Worker:
         # append units
         self._units = [
             "system_unit",
-            "led_unit",
-            "gpio_unit",
-            "weight_unit",
-            "co2_sensor_unit",
-            "temp_sensor_unit",
-            "k30_unit"
+            "ard_unit"
         ]
         # create units
         self._system_unit = SystemUnit(worker=self)
-        self._led_unit = LedUnit(
-            devname="/dev/ttyUSB1"
-        )
-        self._k30_unit = K30Unit(
-            devname="/dev/ttyUSB2"
-        )
-        self._gpio_unit = GpioUnit()
-        self._co2_sensor_unit = CO2SensorUnit(devname="/dev/ttyUSB0")
-        self._weight_unit = WeightUnit()
-        self._temp_sensor_unit = TempSensorUnit()
+        # self._led_unit = LedUnit(
+        #     devname="/dev/ttyUSB1"
+        # )
+        # self._k30_unit = K30Unit(
+        #     devname="/dev/ttyUSB2"
+        # )
+        # self._gpio_unit = GpioUnit()
+        # self._co2_sensor_unit = CO2SensorUnit(devname="/dev/ttyUSB0")
+        # self._weight_unit = WeightUnit()
+        # self._temp_sensor_unit = TempSensorUnit()
         # create stubs for periodic tasks
-        self.measure_task = None
-        self.schedule_task = None
+        # self.measure_task = None
+        # self.schedule_task = None
         self.request_task = None
         self.send_results_task = None
 
@@ -106,46 +100,45 @@ class Worker:
         logger.info("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
         logger.info("New epoch started!")
         logger.info("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
-        # find config and check it
-        try:
-            with open("current.config") as f:
-                raw_data = f.read()
-                data = raw_data.split(":")
-                self.cycle = int(data[0])
-                self.current_schedule_point = int(data[1])
-                logger.info("Config file found, data loaded")
-        except Exception as e:
-            logger.error("Error while reading config: {}".format(e))
-            logger.error("Creating new default config")
-            with open("current.config", "w") as f:
-                f.write("{}:{}".format(self.cycle, self.current_schedule_point))
+        # # find config and check it
+        # try:
+        #     with open("current.config") as f:
+        #         raw_data = f.read()
+        #         data = raw_data.split(":")
+        #         self.cycle = int(data[0])
+        #         self.current_schedule_point = int(data[1])
+        #         logger.info("Config file found, data loaded")
+        # except Exception as e:
+        #     logger.error("Error while reading config: {}".format(e))
+        #     logger.error("Creating new default config")
+        #     with open("current.config", "w") as f:
+        #         f.write("{}:{}".format(self.cycle, self.current_schedule_point))
 
         # start all things, those need to be done once
-        await self._gpio_unit.start_coolers()
-        # await self._gpio_unit.start_draining()
-        await self._gpio_unit.stop_draining()
-        await self._led_unit.set_current(red=10, white=10)
-        # we have to start air pump 3 before all
-        # TODO: think how to work with pump 3 normally
-        pump3_pin = self._gpio_unit.measure_pins # it is list, yeah its dumb
-        for i in pump3_pin:
-            await self._gpio_unit.set_pin(pin=i, state=False)
-        # do async init for some units
-        await self._co2_sensor_unit.init() # for time
+        # await self._gpio_unit.start_coolers()
+        # # await self._gpio_unit.start_draining()
+        # await self._gpio_unit.stop_draining()
+        # await self._led_unit.set_current(red=10, white=10)
+        # # we have to start air pump 3 before all
+        # # TODO: think how to work with pump 3 normally
+        # pump3_pin = self._gpio_unit.measure_pins # it is list, yeah its dumb
+        # for i in pump3_pin:
+        #     await self._gpio_unit.set_pin(pin=i, state=False)
+        # # do async init for some units
+        # await self._co2_sensor_unit.init() # for time
         # that tasks is not user`s, so they not in self._tasks
         self._main_loop_task = asyncio.ensure_future(self._run_main_loop())
-        self.schedule_task = PeriodicCoro(self.check_schedule, 3, name="schedule_task")
         # # TODO: remove it in future
         # self.schedule_task = PeriodicCoro(self.passive_schedule, 3, name="schedule_task")
         # self.schedule_task = PeriodicCoro(self.one_shot_schedule, 3, name="schedule_task")
 
         self.request_task = PeriodicCoro(self.check_server, 3, name="request_task")
         self.send_results_task = PeriodicCoro(self.send_results, 3, name="send_results_task")
-        self.measure_task = PeriodicCoro(self.measure, 1, name="measure_task")
-        await self.schedule_task.start()
+        # self.measure_task = PeriodicCoro(self.measure, 1, name="measure_task")
+        # await self.schedule_task.start()
         await self.request_task.start()
         await self.send_results_task.start()
-        await self.measure_task.start()
+        # await self.measure_task.start()
         self._started = True
         logger.info("worker started")
 
@@ -154,11 +147,11 @@ class Worker:
 
     async def stop(self):
         # stop all units and event loop
-        await self._gpio_unit.stop()
-        await self.schedule_task.stop()
+        # await self._gpio_unit.stop()
+        # await self.schedule_task.stop()
         await self.request_task.stop()
         await self.send_results_task.stop()
-        await self.measure_task.stop()
+        # await self.measure_task.stop()
         self._main_loop_task.cancel()
         # TODO: mb add stopping led driver?
         self._started = False
@@ -166,13 +159,12 @@ class Worker:
         with suppress(asyncio.CancelledError):
             await self._main_loop_task
 
-
     async def pause(self):
         # stop scheduling and measurements
         if self._started:
             logger.info("MANUAL COMMAND: worker auto regime paused")
             await self.schedule_task.stop()
-            await self.measure_task.stop()
+            # await self.measure_task.stop()
             self._started = False
 
     async def continue_(self):
@@ -180,32 +172,8 @@ class Worker:
         if not self._started:
             logger.info("MANUAL COMMAND: worker auto regime continued")
             await self.schedule_task.start()
-            await self.measure_task.start()
+            # await self.measure_task.start()
             self._started = True
-
-    async def start_ventilation(self):
-        # start ventilation forever
-        logger.info("MANUAL COMMAND: start ventilation")
-        res = ""
-        res += await self._gpio_unit.start_ventilation()
-        return res
-
-    async def stop_ventilation(self):
-        # stop ventilation
-        logger.info("MANUAL COMMAND: stop ventilation")
-        res = ""
-        res += await self._gpio_unit.stop_ventilation()
-        return res
-
-    async def do_calibration(self):
-        # do calibration once
-        logger.info("MANUAL COMMAND: do calibration once")
-        res = ""
-        res += await self._gpio_unit.start_calibration()
-        res += await self._co2_sensor_unit.do_calibration()
-        await asyncio.sleep(self._calibration_time)
-        res += await self._gpio_unit.stop_calibration()
-        return res
 
     async def _run_main_loop(self):
         # TODO: mb here must be nothing, and we should put all things to another PeriodicCoro?
@@ -260,20 +228,20 @@ class Worker:
         elif com.unit == "system_unit":
             new_task = await self._system_unit.handle_ticket(tick)
 
-        elif com.unit == "led_unit":
-            new_task = await self._led_unit.handle_ticket(tick)
-
-        elif com.unit == "gpio_unit":
-            new_task = await self._gpio_unit.handle_ticket(tick)
-
-        elif com.unit == "weight_unit":
-            new_task = await self._weight_unit.handle_ticket(tick)
-
-        elif com.unit == "co2_sensor_unit":
-            new_task = await self._co2_sensor_unit.handle_ticket(tick)
-
-        elif com.unit == "temp_sensor_unit":
-            new_task = await self._temp_sensor_unit.handle_ticket(tick)
+        # elif com.unit == "led_unit":
+        #     new_task = await self._led_unit.handle_ticket(tick)
+        #
+        # elif com.unit == "gpio_unit":
+        #     new_task = await self._gpio_unit.handle_ticket(tick)
+        #
+        # elif com.unit == "weight_unit":
+        #     new_task = await self._weight_unit.handle_ticket(tick)
+        #
+        # elif com.unit == "co2_sensor_unit":
+        #     new_task = await self._co2_sensor_unit.handle_ticket(tick)
+        #
+        # elif com.unit == "temp_sensor_unit":
+        #     new_task = await self._temp_sensor_unit.handle_ticket(tick)
 
         if new_task:
             logger.debug("parse_ticket is awaiting _tasks_lock!")
@@ -341,213 +309,6 @@ class Worker:
         # TODO: do real archiving
         pass
 
-    async def passive_schedule(self):
-        """
-        simple schedule with constant parameter
-        :return:
-        """
-        logger.debug("Passive_schedule")
-        # constant point [500, 1.5]
-        red = 115
-        white = 58
-
-        period = 30 # mins
-
-        if not self._calibration_lock.locked():
-            t = time.localtime()
-            if t.tm_min % period == 0:
-                remake_coro = SingleCoro(
-                    self.remake,
-                    "recalibration_task",
-                    red=red,
-                    white=white
-                )
-                await remake_coro.start()
-
-    async def check_schedule(self):
-        """
-        do read schedule
-        and then put tasks to self.tickets[] (with lock)
-        hehe, nope
-        :return:
-        """
-        logger.debug("check_schedule")
-        # period = 30 # in mins
-        period = 30  # in mins
-        # sched = [
-        #     [10, 258],  # 700, 0
-        #     [10, 69],  # 200, 0
-        #     [10, 163],  # 450, 0
-        #     [166, 133],  # 700, 1
-        #     [46, 38],  # 200, 1
-        #     [106, 85],  # 450, 1
-        #     [199, 106],  # 700, 1.5
-        #     [56, 30],  # 200, 1.5
-        #     [128, 68],  # 450, 1.5
-        #     [166, 133],  # 700, 1  ------------------- repeating
-        #     [106, 85],  # 450, 1
-        #     [46, 38],  # 200, 1
-        #     [128, 68],  # 450, 1.5
-        #     [199, 106],  # 700, 1.5
-        #     [56, 30],  # 200, 1.5
-        #     [10, 258],  # 700, 0
-        #     [10, 163],  # 450, 0
-        #     [10, 69]  # 200, 0
-        # ]
-        # TODO: remove after end of transients research
-        sched = [
-            [10, 258, 10],  # 700, 0
-            [10, 163, 10],  # 450, 0
-            [10, 258, 10],  # 700, 0
-            [199, 106, 10],  # 700, 1.5
-            [128, 68, 10],  # 450, 1.5
-            [199, 106, 10],  # 700, 1.5
-            [10, 258, 10],  # 700, 0
-            [199, 106, 10],  # 700, 1.5
-            [128, 68, 0],  # 450, 1.5
-            [128, 68, 2],  # 450, 1.5
-            [128, 68, 5],  # 450, 1.5
-            [128, 68, 10],  # 450, 1.5
-            [128, 68, 15],  # 450, 1.5
-            [128, 68, 20]  # 450, 1.5
-        ]
-
-        if not self._calibration_lock.locked():
-            # TODO: find here the cause of mistake in numbering of data in csv file
-            # mb it >=  ??? mb change to >
-            if self.current_schedule_point >= len(sched):
-                self.current_schedule_point = self.current_schedule_point % len(sched)
-                self.cycle += 1
-            t = time.localtime()
-            # if t.tm_min % period == 0:
-            # TODO: remove after end of transients research
-            if t.tm_hour % 1 == 0 and t.tm_min == 11:
-                remake_coro = SingleCoro(
-                    self.remake,
-                    "recalibration_task",
-                    red=sched[self.current_schedule_point][0],
-                    white=sched[self.current_schedule_point][1],
-                    period=sched[self.current_schedule_point][2]
-                )
-                await remake_coro.start()
-                self.current_schedule_point += 1
-                logger.info("Writing data to config")
-                with open("current.config", "w") as f:
-                    f.write("{}:{}".format(self.cycle, self.current_schedule_point))
-            # TODO: remove after end of transients research
-            else:
-                if t.tm_min % 20 == 0:
-                    simple_calibration_coro = SingleCoro(
-                        self.simple_calibration,
-                        "simple_calibration_task"
-                    )
-                    await simple_calibration_coro.start()
-
-        # if self._search_lock.locked():
-        #     self._search_lock.release()
-        # self._search_done = True
-        # # TODO: set max optimal current here
-
-    async def one_shot_schedule(self):
-        # TODO: fix that, it doesnt really work
-        t = time.localtime()
-        if not self._search_lock.locked():
-            if t.tm_min == 0 and t.tm_hour == 1 or not self._search_done:
-                self._search_lock.locked()
-                one_shot_sched_coro = SingleCoro(
-                    self.check_schedule,
-                    "one_shot_sched_task"
-                )
-                await one_shot_sched_coro.start()
-
-    async def simple_calibration(self):
-        await self._calibration_lock.acquire()
-        logger.info("Simple calibration started")
-        res = ""
-        await self.measure_task.stop()
-        res += await self._gpio_unit.start_calibration()
-        res += await self._co2_sensor_unit.do_calibration()
-        await asyncio.sleep(self._calibration_time)
-        res += await self._gpio_unit.stop_calibration()
-        await self.measure_task.start()
-        self._calibration_lock.release()
-        return res
-
-    async def remake(self, red: int, white: int, period: int):
-        await self._calibration_lock.acquire()
-        logger.info("Airflow and calibration started")
-        res = ""
-        res += await self._gpio_unit.start_draining()
-        # res += await self._led_unit.set_current(red=red, white=white)
-        logger.info("New red and white currents is {} and {}".format(red, white))
-        res += await self._gpio_unit.start_ventilation()
-        await self.measure_task.stop()
-        res += await self._gpio_unit.start_calibration()
-        res += await self._co2_sensor_unit.do_calibration()
-        await asyncio.sleep(self._calibration_time)
-        res += await self._gpio_unit.stop_calibration()
-        await self.measure_task.start()
-        await asyncio.sleep(400)
-        await self.measure_task.stop()
-        res += await self._gpio_unit.start_calibration()
-        res += await self._co2_sensor_unit.do_calibration()
-        await asyncio.sleep(self._calibration_time)
-        res += await self._gpio_unit.stop_calibration()
-        await self.measure_task.start()
-        # TODO fix that please
-        await asyncio.sleep(period*60)
-        res += await self._gpio_unit.stop_ventilation()
-
-        # TODO: remove after end of transients research
-        res += await self._led_unit.set_current(red=red, white=white)
-        res += await self._gpio_unit.stop_draining()
-        logger.debug("Result of calibration coro : " + res)
-        self._calibration_lock.release()
-        return res
-
-    async def measure(self):
-        """
-        Get info from all sensors and write it to file
-        :return: dict
-        """
-        logger.debug("measure")
-        # TODO: try to do this function with tickets and handle mechanism
-        date_ = time.strftime("%x", time.localtime())
-        time_ = time.strftime("%X", time.localtime())
-        ired, iwhite = await self._led_unit.get_short_info()
-        temp, hum = await self._temp_sensor_unit.get_data()
-        co2_raw = await self._co2_sensor_unit.do_measurement()
-        co2 = co2_raw.split(' ')[3]
-        k30_co2 = await self._k30_unit.get_data()
-        weight = await self._weight_unit.get_data()
-        # TODO: fix that crutch - air might be set manually
-        if self._calibration_lock.locked():
-            air = 1
-        else:
-            air = 0
-        cyc = self.cycle
-        fieldnames = ["date", "time", "Ired", "Iwhite", "temp", "humid",
-                      "CO2", "weight", "airflow", "cycle", "K30CO2"]
-        data = {
-            "date": date_,
-            "time": time_,
-            "Ired": ired,
-            "Iwhite": iwhite,
-            "temp": temp,
-            "humid": hum,
-            "CO2": co2,
-            "weight": weight,
-            "airflow": air,
-            "cycle": cyc,
-            "K30CO2": k30_co2
-        }
-        async with self._datafile_lock:
-            with open(self._datafile, "a", newline='') as out_file:
-                writer = csv.DictWriter(out_file, delimiter=',', fieldnames=fieldnames)
-                writer.writerow(data)
-
-        return data
-
     async def check_server(self):
         """
         do send request to server, parse answer and put tasks to self.tickets
@@ -584,29 +345,11 @@ class Worker:
             logger.debug("check_server: done")
 
 
-async def non_rpi_main(
-        wid=155167253286217647024261323245457212920,
-        host="83.220.174.247",
-        port=8888
-):
-    # example uuid wid=155167253286217647024261323245457212920
-    # server host 83.220.174.247:8888
-    worker = Worker(wid=wid, host=host, port=port)
-    await worker.start()
-
-if __name__ == "__main__":
-
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(non_rpi_main())
-    loop.run_forever()
-
-
 async def main(
-        wid=155167253286217647024261323245457212920,
+        wid=155167253286217647024261323245457212940,
         host="83.220.174.247",
         port=8888
 ):
-    init(autoreset=True)
     # example uuid wid=155167253286217647024261323245457212920
     # server host 83.220.174.247:8888
     worker = Worker(wid=wid, host=host, port=port)
