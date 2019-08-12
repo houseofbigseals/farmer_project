17b3c658fe15c4d31c28eaa3ab7e1099ad7c8fd0
greencrow <slonik256@yandex.ru>
2019-08-12 23:21:42 +0300

    second real experiment config added

--- low_level_modules/weight_calibration.py
+++ low_level_modules/weight_calibration.py
@@ -44,8 +44,11 @@ full_plants = 35 # 40
 
 # calibration curve
 loads = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100,
-         120, 140, 160, 180, 200, 220, 240, 260, 280, 300
+         120, 140, 160, 180, 200, 220, 240, 260
          ]
 # it is loads added to current tube with plants at zero day
-raw_results = [
+raw_results = [432, 444, 452, 464, 475, 486,
+               493, 502, 511, 522, 532, 554, 574, 595, 618,
+               636, 661, 681, 700, 712
                ]
+# d_weight = +- 1 gramm at each point
